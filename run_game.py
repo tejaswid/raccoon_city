e964ac9fb8e5520017a568db2297b1e5e26d044e
Sundara Tejaswi Digumarti <sundaratejaswi@gmail.com>
2021-03-28 22:12:14 +1100

    Main window done

--- run_game.py
+++ run_game.py
@@ -1,2 +1,29 @@
 import arcade
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Racoon"
+
+class MainGame(arcade.Window):
+
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        arcade.set_background_color(arcade.csscolor.BLUE_VIOLET)
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+
+        arcade.start_render()
+
+
+def main():
+    window = MainGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
