4c9b16b2009764b686b668b54aa8a514d8052625
KrishnaManaswiD <dkrishnamanaswi@gmail.com>
2021-04-03 22:32:33 +0200

    added building tiles

---
+++ resources/maps/buildings.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="buildings" tilewidth="3200" tileheight="896" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="3">
+  <image width="88" height="576" source="../images/backgrounds/lamp.png"/>
+ </tile>
+ <tile id="6">
+  <image width="3200" height="896" source="../images/backgrounds/skyline.png"/>
+ </tile>
+ <tile id="7">
+  <image width="256" height="569" source="../images/backgrounds/house1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="256" height="510" source="../images/backgrounds/house2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="768" height="768" source="../images/backgrounds/tree.png"/>
+ </tile>
+ <tile id="11">
+  <image width="320" height="256" source="../images/backgrounds/house3.png"/>
+ </tile>
+</tileset>
